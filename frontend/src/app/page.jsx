8f51b1bbdb1778291778452d478c29c9dbf12947
YaffetK <yaffet97@gmail.com>
2023-01-24 17:40:58 +0100

    Change backend application properties

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,11 +1,22 @@
-
-import styles from './page.module.css'
+import Navbar from "@/components/Navbar"
 
 
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="min-h-screen flex-grow ">
+      <Navbar />
+
+      <div className="flex w-full flex-col items-center mt-12 flex-grow min-h-fit  ">
+
+
+
+
+      </div>
+
+
+
+
 
     </main>
   )
