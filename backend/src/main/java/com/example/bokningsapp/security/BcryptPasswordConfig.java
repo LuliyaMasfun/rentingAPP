ae0c31501841a4d44e7d330c1d98f1bac68945af
YaffetK <yaffet97@gmail.com>
2023-01-26 15:03:14 +0100

    WIP

--- backend/src/main/java/com/example/bokningsapp/security/BcryptPasswordConfig.java
+++ backend/src/main/java/com/example/bokningsapp/security/BcryptPasswordConfig.java
@@ -2,6 +2,7 @@ package com.example.bokningsapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
