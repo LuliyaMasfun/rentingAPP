e3d01f710149906e9ce5d9dee101ef58296e6d0e
YaffetK <yaffet97@gmail.com>
2023-01-26 12:02:05 +0100

    Add image Card  container

--- frontend/src/pages/equipment/index.jsx
+++ frontend/src/pages/equipment/index.jsx
@@ -1,6 +1,8 @@
 import Navbar from '@/components/Navbar'
 import Axios from 'axios'
 import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const API_URL = 'http://localhost:8080/users'
 
@@ -33,7 +35,7 @@ const EquipmentDefault = () => {
 
 
     return (
-        <main className='min-h-screen flex-grow'>
+        <main className='flex min-h-screen flex-grow'>
             <Navbar />
             <div className='flex border-8 m-8'>
                 {data.map(item => (
@@ -45,6 +47,25 @@ const EquipmentDefault = () => {
 
                 ))}
             </div>
+
+            <h1 className='flex text-2xl text-center justify-center mb-1 justify-items-center'>All Equipments</h1>
+
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <Link href="#">
+                    <Image className="rounded-t-lg" width={120} height={120} src={data.map(item => {
+
+                    })} alt="" />
+                </Link>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+
+                </div>
+            </div>
+
 
 
 
