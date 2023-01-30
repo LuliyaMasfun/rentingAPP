84a43df679e2cf5218bf019469aa92295ecf0a3d
lul1ya <luliamasfun1@gmail.com>
2023-01-30 21:20:28 +0100

    crossorigin yaffet

--- backend/src/main/java/com/example/bokningsapp/controller/EquipBookingController.java
+++ backend/src/main/java/com/example/bokningsapp/controller/EquipBookingController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000",maxAge = 3600)
 public class EquipBookingController {
     private final EquipBookingService equipBookingService;
    
