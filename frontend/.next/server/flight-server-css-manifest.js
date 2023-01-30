0d829afdeb20e5b07f7a85bae4593eb3e42c37d4
YaffetK <yaffet97@gmail.com>
2023-01-30 19:52:30 +0100

    Try adding files nothing works at the moment

---
+++ frontend/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "C:\\Java\\bookingapp\\rentingApp\\frontend\\src\\app\\page": [
+      "C:\\Java\\bookingapp\\rentingApp\\frontend\\src\\app\\globals.css"
+    ]
+  },
+  "C:\\Java\\bookingapp\\rentingApp\\frontend\\src\\app\\layout.jsx": [
+    "C:\\Java\\bookingapp\\rentingApp\\frontend\\src\\app\\globals.css"
+  ]
+}
