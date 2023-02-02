47810f2ff9b173466cd72f2111bf3c70262f3417
Luliya Masfun <luliyamasfun@MacBook-Pro-som-tillhor-Luliya.local>
2023-02-02 03:23:42 +0100

    sign in page design done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -64,30 +64,92 @@ const InputPassword = styled.input`
   ::placeholder {
     color: #fff;
 `;
+const ForgotPasswordLink = styled.a`
+position: absolute;
+margin-top: 20vh;
+margin-left: 20vh;
+font-size: 12px;
+text-decoration: underline;
+cursor: pointer;
+color: #FFFFFF;
+&:hover {
+  color: #777;
+}
+`;
 
-const Button = styled.button`
+const ButtonLogin = styled.button`
+position: absolute;
  border: none;
- margin-top: 412px;
+ margin-top: 350px;
   padding: 10px;
-  width: 260px;
+  width: 275px;
   background-color:white;
   color: #1E1E1E;
   font-weight: 700;
   font-size: 16px;
   border-radius: 5px;
   box-shadow: 0px 3px rgba(0, 0, 0, 0.3);
-`
-const ForgotPasswordLink = styled.a`
-text-decoration: underline;
-cursor: pointer;
-color: #FFFFF;
+`;
 
-&:hover {
-  color: #777;
-}
+const ButtonBankid = styled.div`
+position:absolute;
+border: 1px solid white;
+ margin-top: 550px;
+  padding: 10px;
+  width: 255px;
+  font-weight: 400;
+  font-size: 16px;
+  border-radius: 5px;
+  box-shadow: 0px 3px rgba(0, 0, 0, 0.3);
+  color: white;
+  text-align: center;
+
+`;
+const Border1 = styled.div`
+display: flex;
+flex-direction: row;
+  height: 0px;
+  width: 100px;
+  border: 1px solid white;
+  margin-left: -20vh;
+  margin-top: 55vh;
+  opacity: 0.5;
+
+`;
+const Or = styled.p`
+display: flex;
+flex-direction: row;
+color: white;
+margin-left: 0vh;
+margin-top: -1vh;
+font-weight: 300;
+font-size: 14px;
+
+`;
+const Border2 = styled.div`
+display: flex;
+flex-direction: row;
+height: 0px;
+width: 100px;
+border: 1px solid white;
+margin-left: 20vh;
+margin-top: -2.8vh;
+opacity: 0.5;
+`;
+
+const UnderlinedText = styled.span`
+  text-decoration: underline;
 `;
 
 
+const SignUp = styled.span`
+position:absolute;
+margin-top: 75vh;
+font-weight: 300;
+font-size: 12px;
+color: white;
+
+`;
 
 
 
@@ -110,12 +172,12 @@ const Login = () => {
     // Add code to send the form data to the server and authenticate the user
   }
 
-  const history = useHistory();
+  /*const history = useHistory();
 
   function handleForgotPassword() {
     history.push('/reset-password');
   }
-
+*/
 
 
   return (
@@ -123,28 +185,35 @@ const Login = () => {
     <Page>
       <Title>Welcome Back,</Title>
       <Subtitle>please enter your credentials</Subtitle>
-    <Form onSubmit={handleSubmit}>
-      <InputEmail
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
-        css={{ color: 'hotpink' }}
-      />
-      <InputPassword 
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-       <ForgotPasswordLink onClick={handleForgotPassword}>
-      Forgot password
-    </ForgotPasswordLink>
-    
-      <Button type="submit">Log in</Button>
-    </Form>
+      <Form onSubmit={handleSubmit}>
+        <InputEmail
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          css={{ color: 'hotpink' }}
+        />
+        <InputPassword
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <ForgotPasswordLink /*onClick={handleForgotPassword}*/>
+          Forgot password
+        </ForgotPasswordLink>
+
+        <ButtonLogin type="submit">Log in</ButtonLogin>
+        <Border1 />
+        <Or>or</Or>
+        <Border2 />
+        <ButtonBankid type="submit">Log in with Bank id</ButtonBankid>
+        <SignUp /*onClick={handleSignUp}*/>
+          Dont have an account? <UnderlinedText>Sign up</UnderlinedText>
+        </SignUp>
+      </Form>
     </Page>
   )
 }
