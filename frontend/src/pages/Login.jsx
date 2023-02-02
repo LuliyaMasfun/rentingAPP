bce0e6a4019a67108c9316fa77cd21404277d4e0
Luliya Masfun <luliyamasfun@MacBook-Pro-som-tillhor-Luliya.local>
2023-02-02 02:21:48 +0100

    Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,98 @@
 
 import React, { useState } from 'react'
-import { css, styled } from '@emotion/react';
+import styled from "@emotion/styled";
+import { useHistory } from 'react-router-dom';
+
+const Page = styled.div`
+  height: 100vh;
+  background-image: url('/bgImgSignUp.png');
+  background-size: cover;
+  background-position: center;
+  background-color: #1E1E1E;
+`;
+
+const Title = styled.h1`
+  position: absolute;
+  margin-top:14vh;
+  margin-left: 6vh;
+  font-size: 28px;
+  font-weight: 500;
+  margin-bottom: 20px;
+  color: white;
+`;
+
+const Subtitle = styled.p`
+  position: absolute;
+  margin-top:18vh;
+  margin-left: 6vh;
+  font-size: 18px;
+  margin-bottom: 40px;
+  color:white;
+`;
+
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+`;
+
+const InputEmail = styled.input`
+  position: absolute;
+  margin-top: -30px;
+  border: none;
+  padding: 10px;
+  width: 260px;
+  background: transparent;
+  border-bottom: 1px solid white;
+  color: white;
+  ::placeholder {
+    color: #fff;
+`;
+
+const InputPassword = styled.input`
+ position: absolute;
+ margin-top: 100px;
+  border: none;
+  padding: 10px;
+  width: 260px;
+  background: transparent;
+  border-bottom: 1px solid white;
+  color: white;
+  ::placeholder {
+    color: #fff;
+`;
+
+const Button = styled.button`
+ border: none;
+ margin-top: 412px;
+  padding: 10px;
+  width: 260px;
+  background-color:white;
+  color: #1E1E1E;
+  font-weight: 700;
+  font-size: 16px;
+  border-radius: 5px;
+  box-shadow: 0px 3px rgba(0, 0, 0, 0.3);
+`
+const ForgotPasswordLink = styled.a`
+text-decoration: underline;
+cursor: pointer;
+color: #FFFFF;
+
+&:hover {
+  color: #777;
+}
+`;
+
+
+
+
+
+
+
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -18,30 +110,43 @@ const Login = () => {
     // Add code to send the form data to the server and authenticate the user
   }
 
+  const history = useHistory();
+
+  function handleForgotPassword() {
+    history.push('/reset-password');
+  }
+
+
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input 
+
+    <Page>
+      <Title>Welcome Back,</Title>
+      <Subtitle>please enter your credentials</Subtitle>
+    <Form onSubmit={handleSubmit}>
+      <InputEmail
         type="email"
         name="email"
         placeholder="Email"
         value={formData.email}
         onChange={handleChange}
+        css={{ color: 'hotpink' }}
       />
-      <input 
+      <InputPassword 
         type="password"
         name="password"
         placeholder="Password"
         value={formData.password}
         onChange={handleChange}
       />
-      <button type="submit">Login</button>
-    </form>
+       <ForgotPasswordLink onClick={handleForgotPassword}>
+      Forgot password
+    </ForgotPasswordLink>
+    
+      <Button type="submit">Log in</Button>
+    </Form>
+    </Page>
   )
-
-  const Button = styled.button`
-  border: none;
-  `
 }
 
 export default Login
