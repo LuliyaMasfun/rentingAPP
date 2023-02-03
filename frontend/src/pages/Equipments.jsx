a896a406c4d6b10cd54560d480c8521b6cb4a2af
Luliya Masfun <luliyamasfun@MacBook-Pro-som-tillhor-Luliya.local>
2023-02-03 14:58:37 +0100

    soon done with equipment page :)

--- frontend/src/pages/Equipments.jsx
+++ frontend/src/pages/Equipments.jsx
@@ -5,18 +5,77 @@ import Router from 'next/router';
 import { FaRegBookmark, FaMapMarkerAlt } from "react-icons/fa";
 import styled from "@emotion/styled";
 import Image from 'next/image';
+import Link from 'next/link';
 import light from "../../public/aputure.png";
 import sound from "../../public/rode.png";
 import camera from "../../public/canon.png";
 
+
 const Page = styled.div`
-  height: 1584px;
+  position: absolute;
+  height: 1384px;
   width: 390px;
   background-color: #1E1E1E;
+  margin:0;
+`;
+
+const PageTitle = styled.h1`
+position: absolute;
+font-size: 20px;
+font-weight: 600;
+margin-top: 55px;
+margin-left: 35px;
+color: white;
+`;
+
+const ViewAll = styled.button`
+position:absolute;
+margin-top: 110px;
+margin-left: 35px;
+width: 70px;
+color:white;
+font-size: 12px;
+background-color: transparent;
+border: solid  1px white;
+border-radius: 5px;
+
+`;
+const SoundFilter = styled.button`
+position:absolute;
+margin-top: 110px;
+margin-left: 115px;
+width: 70px;
+color:white;
+font-size: 12px;
+background-color: transparent;
+border: solid  1px white;
+border-radius: 5px;
+`;
+const LightFilter = styled.button`
+position:absolute;
+margin-top: 110px;
+margin-left: 195px;
+width: 70px;
+color:white;
+font-size: 12px;
+background-color: transparent;
+border: solid  1px white;
+border-radius: 5px;
+`;
+const CameraFilter = styled.button`
+position:absolute;
+margin-top: 110px;
+margin-left: 275px;
+width: 70px;
+color:white;
+font-size: 12px;
+background-color: transparent;
+border: solid  1px white;
+border-radius: 5px;
 `;
 
 const Container = styled.div`
-margin-top: 140px;
+margin-top: 170px;
  display: flex;
   flex-direction: column;
 `;
@@ -26,11 +85,11 @@ margin-bottom: 275px;
 display: flex;
 flex-direction: row;
 justify-content: center;
+
 `;
 const EquipmentImage = styled(Image)`
 position: absolute;
 z-index: 0;
-margin-left: 2vh;
 `;
 
 const BookmarkIcon = styled(FaRegBookmark)`
@@ -47,36 +106,37 @@ z-index:2;
 border: 1px solid white;
 width: 326px;
 margin-top: 23vh;
-margin-left: 2vh;
 `;
 
 const Name = styled.p`
 position: absolute;
 z-index:2;
 margin-top: 20vh;
-margin-left: -18vh;
+margin-left: -15vh;
 font-size: 16px;
 font-weight: 700;
 color: white;
+width: 160px;
 `;
 
 const LocationIcon = styled(FaMapMarkerAlt)`
 position: absolute;
 z-index:2;
 margin-top: 24vh;
-margin-left: -30vh;
+margin-left: -32vh;
 color: white;
 `;
 const LocationTxt = styled.p`
 position: absolute;
 z-index:2;
 margin-top: 23.6vh;
-margin-left: -14vh;
+margin-left: -16vh;
 color:white;
 `;
 
 const EquipmentPage = () => {
   const [data, setData] = useState([]);
+  const [filter, setFilter] = useState('all');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -91,34 +151,57 @@ const EquipmentPage = () => {
     fetchData();
   }, []);
 
-  const checkType = (item) => {
+  const handleFilter = (type) => {
+    setFilter(type);
+  };
 
-    if (item.equipmentType == "CAMERA") {
+  const filteredEquipment = data.filter((item) => {
+    if (filter === 'all') {
+      return data;
+    } else {
+      return data.equipmentType === filter.toUpperCase()
+    }
+  });
+
+
+  const checkType = (imageType) => {
+
+    if (imageType.equipmentType == "CAMERA") {
       return camera;
-    } else if (item.equipmentType == "LIGHT") {
+    } else if (imageType.equipmentType == "LIGHT") {
       return light;
     } else {
       return sound;
     }
   }
 
+
   return (
-    <Container>
-      {data ? data.map((item) => (
-        <Card key={item.id}>
-          <BookmarkIcon />
-          <Border />
-          <Name>
-            {item.equipmentName}
-          </Name>
-          <LocationIcon />
-          <LocationTxt>
-            {item.equipmentLocation}
-          </LocationTxt>
-          <EquipmentImage src={checkType(item)} />
-        </Card>
-      )) : <p>Loading...</p>}
-    </Container>
+    <Page>
+      <PageTitle>Equipment</PageTitle>
+      <ViewAll onClick={() => handleFilter('all')}>View all</ViewAll>
+      <SoundFilter onClick={() => handleFilter('SOUND')}>Sound</SoundFilter>
+      <LightFilter onClick={() => handleFilter('LIGHT')}>Light</LightFilter>
+      <CameraFilter onClick={() => handleFilter('CAMERA')}>Camera</CameraFilter>
+      <Container>
+
+        {/* Map through filtered equipment and render the cards */}
+        {filteredEquipment.map((item) => (
+          <Card key={item.id}>
+            <BookmarkIcon />
+            <Border />
+            <Name>
+              {item.equipmentName}
+            </Name>
+            <LocationIcon />
+            <LocationTxt>
+              {item.equipmentLocation}
+            </LocationTxt>
+            <EquipmentImage src={checkType(item)} />
+          </Card>
+        ))}
+      </Container>
+    </Page>
   );
 };
 export default EquipmentPage;
