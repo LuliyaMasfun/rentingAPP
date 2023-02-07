6db23ae368cfaad30fad97521269336f117e366b
YaffetK <yaffet97@gmail.com>
2023-02-07 20:29:48 +0100

    WIP

--- backend/src/main/java/com/example/bokningsapp/BokningsappApplication.java
+++ backend/src/main/java/com/example/bokningsapp/BokningsappApplication.java
@@ -10,19 +10,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+
 @SpringBootApplication
 public class BokningsappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BokningsappApplication.class, args);
 	}
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build();
-	}
+
 }
