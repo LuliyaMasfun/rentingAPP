c8111190331f489eec5b80016d6e6d06977d81b0
Luliya Masfun <luliyamasfun@MacBook-Pro-som-tillhor-Luliya.local>
2023-02-07 19:51:54 +0100

    downgrading next

--- frontend/src/pages/Equipments.jsx
+++ frontend/src/pages/Equipments.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
@@ -142,6 +143,8 @@ margin-top: 23.6vh;
 margin-left: -16vh;
 color:white;
 `;
+
+
 
 const EquipmentPage = () => {
   const [data, setData] = useState([]);
